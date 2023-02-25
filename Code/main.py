cd7b27804a438d0039451a2d26ee0758c68d9971
samuelladoco <26620703+samuelladoco@users.noreply.github.com>
2023-02-26 07:58:46 +0900

    Add files via upload

--- Code/main.py
+++ Code/main.py
@@ -13,35 +13,46 @@ import util
 
 # Version No.
 # -----------------------------------------------------------------------------
-ver_no: str = '0.3'
+ver_no: str = '0.4'
 # -----------------------------------------------------------------------------
 
 
 # Main
 # -----------------------------------------------------------------------------
-n: str = f'--- Twitch EventSub Response Bot (v{ver_no}) ---'
-print(f'{n}')
-#
-print(f'[Preprocess]')
-j_file: pathlib.Path = (
-    pathlib.Path(
-        rf'{os.path.abspath(os.path.dirname(sys.argv[0]))}'
-    ).joinpath('config.json5')
-)
-print(f'  JSON5 file path = {j_file}')
-print(f'    parsing this file ... ', end='', )
-j_obj: dict[str, Any] = util.JSON5Reader.open_and_loads(j_file)
-j_obj['ver_no'] = ver_no
-print(f'done.')
-print(f'')
-#
-print(f'[Activation of Bot]')
-b: ter.TERBot = ter.TERBot(j_obj)
-#
-print(f'[Running of Bot]')
-b.run()
-#
-print(f'[Postprocess] (nothing)')
-print(f'')
-print(f'-' * len(n))
+# ----------------------------------------------------------------------
+def main() -> int:
+    n: str = f'--- Twitch EventSub Response Bot (v{ver_no}) ---'
+    print(f'{n}')
+    #
+    print(f'[Preprocess]')
+    j_file: pathlib.Path = (
+        pathlib.Path(
+            rf'{os.path.abspath(os.path.dirname(sys.argv[0]))}'
+        ).joinpath('config.json5')
+    )
+    print(f'  JSON5 file path = {j_file}')
+    print(f'    parsing this file ... ', end='', )
+    j_obj: dict[str, Any] = util.JSON5Reader.open_and_loads(j_file)
+    j_obj['ver_no'] = ver_no
+    print(f'done.')
+    print(f'')
+    #
+    print(f'[Activation of Bot]')
+    b: ter.TERBot = ter.TERBot(j_obj)
+    #
+    print(f'[Run of Bot]')
+    b.run()
+    #
+    print(f'[Postprocess] (nothing)')
+    print(f'')
+    print(f'-' * len(n))
+    return b.return_code
+# ----------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+    return_code: int = main()
+    sys.exit(return_code)
+# ----------------------------------------------------------------------
 # -----------------------------------------------------------------------------
